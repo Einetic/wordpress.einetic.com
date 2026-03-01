cf67c77b5062d586c511d21c1d44679dba8f7f47
afjal-einetic <afjal.einetic@gmail.com>
2026-03-01 06:31:41 +0530

    Update list-sites.sh

--- util/list-sites.sh
+++ util/list-sites.sh
@@ -3,28 +3,22 @@
 SITE_PATHS=()
 i=1
 
-# Hostinger
 for dir in "$HOME"/domains/*/public_html; do
     if [ -f "$dir/wp-config.php" ]; then
-        domain=$(basename "$(dirname "$dir")")
+        domain=$(basename "$(dirname "$dir"))
         echo "$i. $domain"
         SITE_PATHS+=("$dir")
         ((i++))
     fi
 done
 
-# CloudPanel
-for dir in /home/*/htdocs/*/; do
-    if [ -f "$dir/wp-config.php" ]; then
-        domain=$(basename "$dir")
-        echo "$i. $domain"
-        SITE_PATHS+=("$dir")
-        ((i++))
-    fi
-done
+if [ ${#SITE_PATHS[@]} -eq 0 ]; then
+    echo "No WordPress sites found."
+    exit
+fi
 
 echo "0. Back"
-
+echo "====================="
 read -p "Select site: " choice
 
 if [ "$choice" -eq 0 ]; then
