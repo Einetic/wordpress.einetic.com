6cb23ef6ef8043f5c672c60f0de76cdbaeac69fc
afjal-einetic <afjal.einetic@gmail.com>
2026-03-03 11:45:52 +0530

    Update core.sh

--- util/core.sh
+++ util/core.sh
@@ -22,3 +22,23 @@ wp_exec() {
         exit 1
     fi
 }
+
+safe_wp() {
+
+SITE="$1"
+shift
+CMD="$@"
+
+WP_CONTENT="$SITE/wp-content"
+
+[ -f "$WP_CONTENT/object-cache.php" ] && mv "$WP_CONTENT/object-cache.php" "$WP_CONTENT/object-cache.php.bak"
+[ -f "$WP_CONTENT/advanced-cache.php" ] && mv "$WP_CONTENT/advanced-cache.php" "$WP_CONTENT/advanced-cache.php.bak"
+[ -d "$WP_CONTENT/mu-plugins" ] && mv "$WP_CONTENT/mu-plugins" "$WP_CONTENT/mu-plugins.bak"
+
+wp_exec "$SITE" $CMD --skip-plugins --skip-themes
+
+[ -f "$WP_CONTENT/object-cache.php.bak" ] && mv "$WP_CONTENT/object-cache.php.bak" "$WP_CONTENT/object-cache.php"
+[ -f "$WP_CONTENT/advanced-cache.php.bak" ] && mv "$WP_CONTENT/advanced-cache.php.bak" "$WP_CONTENT/advanced-cache.php"
+[ -d "$WP_CONTENT/mu-plugins.bak" ] && mv "$WP_CONTENT/mu-plugins.bak" "$WP_CONTENT/mu-plugins"
+
+}
